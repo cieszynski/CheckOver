f6a03f9cdbea223eca03afb01e5021f22f3efaef
cieszynski <admin@cieszynski.de>
2025-06-18 15:38:25 +0200

    animation timer added

--- checkover.js
+++ checkover.js
@@ -2,7 +2,6 @@ onload = (event) => {
     if (localStorage.length) {
         document.querySelectorAll(".durable")
             .forEach((elem) => {
-                
                 switch (elem.type) {
                     case "checkbox":
                     case "radio":
@@ -17,13 +16,21 @@ onload = (event) => {
                             );
                         }
                         break;
+
+                    default:
+                        elem.value = localStorage.getItem(elem.id);
                 }
             });
+
+        document.body.getAnimations().forEach((animation) => {
+            animation.currentTime = parseFloat(
+                localStorage.getItem("currentTime"),
+            );
+        });
     }
 };
 
 data.oninput = (event) => {
-    console.log(event);
     switch (event.target.type) {
         case "radio":
             data.elements[event.target.name]
@@ -40,5 +47,47 @@ data.oninput = (event) => {
                 event.target.checked,
             );
             break;
+
+        default:
+            localStorage.setItem(
+                event.target.id,
+                event.target.value,
+            );
     }
 };
+document.body.onanimationiteration = (event) => {
+    localStorage.setItem("currentTime", event.elapsedTime * 1000);
+};
+
+document.body.onanimationend = (event) => {
+    //alert(8)
+};
+
+document.addEventListener("keydown", (event) => {
+    if (event.ctrlKey && event.shiftKey) {
+        switch (event.key) {
+            case "F5":
+                event.preventDefault();
+
+                document.body.getAnimations().forEach((animation) => {
+                    animation.finish();
+                });
+                localStorage.clear();
+                break;
+            default:
+                console.log(event);
+        }
+    } else if (event.ctrlKey) {
+        switch (event.key) {
+            case "i":
+            case "u":
+                event.preventDefault();
+                break;
+            default:
+                console.log(event);
+        }
+    } else {switch (event.key) {
+            default:
+                console.log(event);
+        }}
+});
